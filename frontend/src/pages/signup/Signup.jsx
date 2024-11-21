3ddce9d1b0532288d15c0c022ab9642b64db5e5e
SashaCHU-st <alexandra.churina@gmail.com>
2024-11-21 10:16:17 +0200

    created SignUp page=> connect to backend

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 import './Signup.css'
 const Signup = () => {
   return (
-    <div>
+    <div className='signup'>
+      <div className='formWr'>
+        <form onSubmit="">
+          <div className='inputWr'>
+          <label htmlFor="name">Name</label>
+          </div>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input className='window' type="text" id ="name"/>
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input className='window' type="text" id='email'/>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input className='window' type="text" id='password' />
+          </div>
+          <button type='submit'>SignUp</button>
+        </form>
+      </div>
       
     </div>
   )
