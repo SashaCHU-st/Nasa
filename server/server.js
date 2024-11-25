ef1793966d4fdf9495a2fce01c9b9f652d0a7fe9
SashaCHU-st <alexandra.churina@gmail.com>
2024-11-25 13:23:15 +0200

    deploying, but few mistakes with db

--- server/server.js
+++ server/server.js
@@ -1,18 +1,11 @@
-const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const path = require("path");
-require("dotenv").config(); // для загрузки переменных окружения
-
+require("dotenv").config();
+const express = require("express");
 const HttpError = require("./models/https-error");
-const UserRoutes = require("./routes/user-routes");
-
 const app = express();
 
-// Middleware для парсинга JSON
+const UserRoutes = require("./routes/user-routes");
 app.use(bodyParser.json());
-
-// CORS настройки (разрешаем запросы с любого источника)
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -23,51 +16,37 @@ app.use((req, res, next) => {
   next();
 });
 
-// Роуты API для работы с пользователями
-app.use("/api/users", UserRoutes);
+const mongoose = require("mongoose");
 
-// Если маршрут не найден, отправляем ошибку 404
+app.use("/api/users", UserRoutes); // даем адрес по которому можно найти
 app.use((req, res, next) => {
-  const err = new HttpError("Could not find this route.", 404);
+  const err = new HttpError("could not found ", 404);
   throw err;
 });
 
-// Глобальная обработка ошибок
 app.use((error, req, res, next) => {
   if (res.headersSent) {
     return next(error);
   }
   res.status(error.code || 500);
   res.json({
-    message: error.message || "An unknown error occurred!",
+    message: error.message || "Anknown error occured!",
   });
 });
 
-// Соединение с MongoDB
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`
-  )
-  .then(() => {
-    console.log("Connected to the database!");
+    )
+    .then(() => {
+      console.log("all good");
+    const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
-    // После подключения к базе данных запускаем сервер
-    const port = process.env.PORT || 10000; // Порт из переменной окружения или 10000 по умолчанию
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
   })
   .catch((err) => {
     console.error("Connection failed:", err);
   });
-
-// Настройки для режима продакшн (если приложение собралось с помощью react-scripts)
-if (process.env.NODE_ENV === "production") {
-  // Отдаем статические файлы из папки build
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  // Для всех других запросов возвращаем index.html, чтобы React-роутинг работал
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+  
