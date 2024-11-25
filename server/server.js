a12b97f712e4c45a61494cc73da63a1e1cab073e
SashaCHU-st <alexandra.churina@gmail.com>
2024-11-25 14:06:20 +0200

    deploying, but few mistakes with db

--- server/server.js
+++ server/server.js
@@ -40,11 +40,7 @@ mongoose
     )
     .then(() => {
       console.log("all good");
-    const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
+    app.listen(5000);
   })
   .catch((err) => {
     console.error("Connection failed:", err);
