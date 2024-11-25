a0dc992be6bee12fa2e59ac14a3a97f21bc10239
SashaCHU-st <alexandra.churina@gmail.com>
2024-11-25 15:28:03 +0200

    deploying, but few mistakes with db

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ mongoose
     )
     .then(() => {
       console.log("all good");
-      const port = process.env.PORT || 5001;  // Используем другой порт (например, 5001)
+      const port = process.env.PORT || 5000; 
       app.listen(port, () => {
         console.log(`Server running on port ${port}`);
       });      
