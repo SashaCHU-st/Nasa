6083244312efd1a34975feeab2a0c9273e518e80
SashaCHU-st <alexandra.churina@gmail.com>
2024-11-25 12:53:12 +0200

    fixing dependencies that required by render

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ mongoose
     )
     .then(() => {
       console.log("all good");
-      const port = process.env.PORT || 5000;
+      const port = process.env.PORT || 10000;
       app.listen(port, () => {
         console.log(`Server running on port ${port}`);
       });
