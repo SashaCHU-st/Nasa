96d35ddb820cf8b94f2b8e97d7d7479065d35475
SashaCHU-st <alexandra.churina@gmail.com>
2025-02-05 14:16:04 +0200

    added TypeScript in my default page

--- client/src/pages/default/Default.tsx
+++ client/src/pages/default/Default.tsx
@@ -8,7 +8,7 @@ const Default = () => {
           <h2>Hello, I am Aleksandra, and I am excited to present my full stack project.</h2>
           <p> This project showcases my skills in building dynamic and responsive web applications. Here's an overview of the technologies I used:</p>
           <ul>
-            <li><strong>Frontend:</strong> Developed using React, interactive user interface.</li>
+            <li><strong>Frontend:</strong> Developed using React with TypeScript, interactive user interface.</li>
             <li><strong>Backend:</strong> Built with Express.js, providing a robust and efficient server-side framework.</li>
             <li><strong>Database:</strong> Integrated with MongoDB to handle data storage and retrieval.</li>
             <li> <strong>API:</strong> Utilized the NASA API to incorporate fascinating space-related data into the application.</li>
