8bf84ababa5094e89eb2c643e55ae295ee270363
SashaCHU-st <alexandra.churina@gmail.com>
2025-03-07 13:04:40 +0200

    returned to previous version with sinple spinner, will fix it later

--- client/src/components/loading/LoadingSpinner.tsx
+++ client/src/components/loading/LoadingSpinner.tsx
@@ -1,24 +1,5 @@
-// import React from "react";
-// import "./LoadingSpinner.css"; 
-
-// interface LoadingSpinnerProps {
-//   asOverlay?: boolean;
-// }
-
-// const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ asOverlay }) => {
-//   return (
-//     <div className={`${asOverlay ? "spinner-overlay" : "spinner"}`}>
-//       <div className="lds-dual-ring">
-//       ‼️Note: It may take some time for the database to connect (1-2 minutes). 
-//       Please wait a moment and refresh the page. ⏳
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LoadingSpinner;
 import React from "react";
-import "./LoadingSpinner.css";
+import "./LoadingSpinner.css"; 
 
 interface LoadingSpinnerProps {
   asOverlay?: boolean;
@@ -26,16 +7,10 @@ interface LoadingSpinnerProps {
 
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ asOverlay }) => {
   return (
-    <div className="spinner-overlay">
-      <p className="loading-message">
-        ‼️ Note: It may take some time for the database to connect (1-2 minutes).  
-        Please wait a moment and refresh the page. ⏳
-      </p>
+    <div className={`${asOverlay ? "spinner-overlay" : "spinner"}`}>
       <div className="lds-dual-ring"></div>
     </div>
   );
 };
 
 export default LoadingSpinner;
-
-
